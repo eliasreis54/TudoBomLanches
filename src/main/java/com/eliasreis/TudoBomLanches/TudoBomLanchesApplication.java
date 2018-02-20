917554f71ddee4720f2654cf45f2029aaddc2d2f
Elias Reis <eliasreis54@gmail.com>
2018-02-19 23:43:33 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/eliasreis/TudoBomLanches/TudoBomLanchesApplication.java
@@ -0,0 +1,12 @@
+package com.eliasreis.TudoBomLanches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TudoBomLanchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TudoBomLanchesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/eliasreis/TudoBomLanches/TudoBomLanchesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eliasreis.TudoBomLanches;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TudoBomLanchesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
